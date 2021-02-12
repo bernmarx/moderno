3869f146639ec0323fc498590cfc28d53108f0e3
bernmarx <d.strelkin@inbox.ru>
2021-02-12 15:08:36 +0700

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(function(){
     var mixer = mixitup('.products_inner-box');
+
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+      });
+
+    $('.product-slider_inner').slick({
+      dots: true,
+      arrows: false,
+      slidesToShow: 4,
+      slidesToScroll: 4
+    });
 });
