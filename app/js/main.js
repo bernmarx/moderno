c0f9733b399c7b067411c542afbb7172f8014426
bernmarx <d.strelkin@inbox.ru>
2021-03-12 16:30:31 +0700

    start new-items + added a bunch of stuff there

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,37 @@
-$(function(){
-    var mixer = mixitup('.products_inner-box');
+$(function () {
 
-    $(".rate-star").rateYo({
-        rating: 5,
-        starWidth: "12px",
-        readOnly: true
-      });
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true
+  });
 
-    $('.product-slider_inner').slick({
-      dots: true,
-      arrows: false,
-      slidesToShow: 4,
-      slidesToScroll: 4
-    });
+  $('.product-slider_inner').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
+
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
+  });
+
+  $('.icon-th-list').on('click', function(){
+    $('.product_item').addClass('list');
+    $('.icon-th-list').addClass('active');
+    $('.icon-th-large').removeClass('active')
+  });
+  $('.icon-th-large').on('click', function(){
+    $('.product_item').removeClass('list');
+    $('.icon-th-large').addClass('active');
+    $('.icon-th-list').removeClass('active')
+  });
+
+  var mixer = mixitup('.products_inner-box');
 });
