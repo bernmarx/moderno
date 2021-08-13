45aca91e9369090ed18fee1c8a7f2c6feb6f77e2
bernmarx <d.strelkin@inbox.ru>
2021-08-13 10:01:45 +0800

    added settings page

--- app/js/main.js
+++ app/js/main.js
@@ -41,10 +41,10 @@ $(function () {
     $('.header_box').toggleClass('active');
   });
 
-  $('.product-one_tabs .tab').on('click', function(event) {
+  $('.product-one_tabs .tab, .settings_tabs .tab').on('click', function(event) {
     var id = $(this).attr('data-id');
-      $('.product-one_tabs').find('.tab-item').removeClass('active-tab').hide();
-      $('.product-one_tabs .tabs').find('.tab').removeClass('active');
+      $('.product-one_tabs, .settings_tabs').find('.tab-item').removeClass('active-tab').hide();
+      $('.product-one_tabs, .settings_tabs .tabs').find('.tab').removeClass('active');
       $(this).addClass('active');
       $('#'+id).addClass('active-tab').fadeIn();
       return false;
